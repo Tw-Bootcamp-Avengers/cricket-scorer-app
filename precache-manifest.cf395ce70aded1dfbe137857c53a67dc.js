f5e276b91b8ce96292e8c7331a329b5898c7a857
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-24 07:35:30 +0000

    Deploy Tw-Bootcamp-Avengers/cricket-scorer-app to github.com/Tw-Bootcamp-Avengers/cricket-scorer-app.git:gh-pages

---
+++ precache-manifest.cf395ce70aded1dfbe137857c53a67dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d001d3fc8b1c5558193d",
+    "url": "/cricket-scorer-app/static/js/runtime~main.d001d3fc.js"
+  },
+  {
+    "revision": "05e525e5193ef1ad3cc4",
+    "url": "/cricket-scorer-app/static/js/main.05e525e5.chunk.js"
+  },
+  {
+    "revision": "600ed7bff914f9bc6930",
+    "url": "/cricket-scorer-app/static/js/1.600ed7bf.chunk.js"
+  },
+  {
+    "revision": "05e525e5193ef1ad3cc4",
+    "url": "/cricket-scorer-app/static/css/main.a1e28195.chunk.css"
+  },
+  {
+    "revision": "600ed7bff914f9bc6930",
+    "url": "/cricket-scorer-app/static/css/1.1b73bbea.chunk.css"
+  },
+  {
+    "revision": "a68762ed18d6a24282f176da46652665",
+    "url": "/cricket-scorer-app/index.html"
+  }
+];
